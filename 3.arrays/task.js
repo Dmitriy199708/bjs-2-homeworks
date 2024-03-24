441486443e7a26061a4d41d69df3f218113ba9bd
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2024-03-24 18:56:52 +0300

    add homework three 24-03-24

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
+"use strict"
+
 function compareArrays(arr1, arr2) {
-  
+    return arr1.every(function (el,index) {
+        if(arr1.length === arr2.length){
+            return el === arr2[index];
+        }
+        return false;
+    });
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let mas = users.filter(item => item.gender === gender).map(item => item.age);
+    let res = mas.reduce((sum, age) => sum + age, 0);
+
+    return (mas.length > 0) ? res / mas.length : 0;
+
 }

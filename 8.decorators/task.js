dfa73e3d00fae13a0f81d0adacab55202dab5fe9
Dmitriy199708 <119492971+Dmitriy199708@users.noreply.github.com>
2024-06-12 20:52:46 +0300

    add project task8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,43 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+        const hash = args.join(','); // получаем правильный хэш
+        let idx = cache.findIndex((item)=> item.hash === hash ); // ищем элемент, хэш которого равен нашему хэшу
+        if (idx !== -1 ) { // если элемент не найден
+            console.log("Из кеша: " + cache[idx].result); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+            return "Из кеша: " + cache[idx].result;
+        }
+
+        let result = func(...args); // в кэше результата нет - придётся считать
+        cache.push({hash, result}) ; // добавляем элемент с правильной структурой
+        if (cache.length > 5) {
+            cache.shift() // если слишком много элементов в кэше надо удалить самый старый (первый)
+        }
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
-//Задача № 2
+
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId;
+    let isTrottled = false;
+    function wrapper(...args) {
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout( () => {
+            func(args);
+            wrapper.count++;
+        }, delay);
+        if (!isTrottled) {
+            func(...args);
+            wrapper.count++;
+            isTrottled = true;
+        }
+        wrapper.allCount++;
+    }
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+    return wrapper;
 }
